a8e3bc3a7ac064bbb96224e082a93f8cd3295c65
Keisuke Shibata <shibata@vision.ist.i.kyoto-u.ac.jp>
2020-09-28 20:27:03 +0900

    fix

--- jupyter_notebook_bg_changer/js/main.js
+++ jupyter_notebook_bg_changer/js/main.js
@@ -3,22 +3,25 @@
 let parser = new URL(document.URL);
 // port: 8888
 let port = parseInt(parser.port);
-const coeff = 130;
-let hue = ((port*coeff)%360).toString();
-let color = "hsl("+hue+",100%, 80%)";
-// parser.pathname: /notebooks/hoge.ipynb
-let page_type = parser.pathname.split("/")[1];
-if (page_type=="notebooks"){
-    // console.log("nb");
-    // console.log(color);
-    document.getElementById("notebook").style.backgroundColor=color;
-}
-else if (page_type=="edit"){
-    // console.log("edit");
-    // console.log(color);
-    document.getElementById("texteditor-backdrop").style.backgroundColor=color;
-    
-}
-else{
-    console.log("jupyter notebook bg changer exit.");
+if (port!=="") {
+    const coeff = 130;
+    let hue = ((port*coeff)%360).toString();
+    let color = "hsl("+hue+",100%, 80%)";
+    // parser.pathname: /notebooks/hoge.ipynb
+    let parsed_url = parser.pathname.split("/");
+    let page_type = parsed_url[1];
+    if (page_type=="notebooks"){
+        // console.log("nb");
+        // console.log(color);
+        document.getElementById("notebook").style.backgroundColor=color;
+    }
+    else if (page_type=="edit"){
+        // console.log("edit");
+        // console.log(color);
+        document.getElementById("texteditor-backdrop").style.backgroundColor=color;
+        
+    }
+    else{
+        console.log("jupyter notebook bg changer exit.");
+    }
 }
